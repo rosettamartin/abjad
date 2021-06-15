08123b1b2c99d9975f8e0bb6b293ae6315939fa2
Rosetta Martin <rsttmrtn@gmail.com>
2021-06-14 20:10:25 -0700

    Surprise it's an abjad now

--- abugida.py
+++ abugida.py
@@ -1,21 +1,18 @@
+import copy
 import os
 from xml.dom import minidom
 from xml.etree import ElementTree
 
 
-class Abugida:
-    def __init__(self, letters):
-        self.letters = letters
-
-    def create_svg(self, letters, min_x='0', min_y='0', w='75', h='75'):
-        svg = ElementTree.Element(
-            'svg',
-            attrib={
-                'viewBox': f'{min_x} {min_y} {w} {h}',
-                'xmlns': 'http://www.w3.org/2000/svg'})
-        for letter in letters:
-            svg.insert(1, self.letters[letter].create_element())
-        return svg
+def create_svg(*shapes, min_x='0', min_y='0', w='75', h='75'):
+    svg = ElementTree.Element(
+        'svg',
+        attrib={
+            'viewBox': f'{min_x} {min_y} {w} {h}',
+            'xmlns': 'http://www.w3.org/2000/svg'})
+    for shape in shapes:
+        svg.insert(1, shape.create_element())
+    return svg
 
 
 class Path:
@@ -62,126 +59,207 @@ class Group:
         return element
 
 
+class Consonant:
+    def __init__(self, shape,
+                 medial_bar=False, final_bar=False, final_shift=None):
+        """
+        A consonant character. `shape` is either a Path, Circle, or
+        Group. The boolean value `medial_bar` specifies whether the
+        consonant's base shape requires a vertical bar. The boolean
+        value `final_bar` specifies whether the consonant's final form
+        requires a vertical bar. `final_shift` is a 2-tuple of x and y
+        values; if provided, the consonant's final form will be shifted
+        by x and y, rather than flipped horizontally.
+
+        :param shape: A Path, Circle, or Group
+        :param medial_bar: A boolean
+        :param final_bar: A boolean
+        :param final_shift: a 2-tuple of x and y values
+        """
+
+        self.shape = shape
+        self.medial_bar = medial_bar
+        self.final_bar = final_bar
+        self.final_shift = final_shift
+
+
 def to_file(svg, filename):
     with open(filename, 'w') as f:
         f.write(minidom.parseString(ElementTree.tostring(svg)).toprettyxml())
 
 
-abugida = Abugida({
-    'null': Path(
-        'M 0 2.5 '
-        'H 16.25 '
-        'V 57.5 '
-        'A 15 15 0 0 1 1.25 72.5 '
-        'H 0'),
-    'base': Path(
+def create_initial_svg(consonant, *vowel):
+    return create_svg(abjad['base_initial'], consonant.shape, *vowel)
+
+
+def create_medial_svg(consonant, *vowel):
+    if consonant.medial_bar:
+        return create_svg(
+            abjad['base_medial_bar'], consonant.shape, *vowel,
+            min_x='-12.5', w='87.5')
+    else:
+        return create_svg(abjad['base_medial'], consonant.shape, *vowel)
+
+
+def create_final_svg(consonant):
+    if consonant.final_bar:
+        new_shape = copy.deepcopy(consonant.shape)
+        # TODO not removing overlap properly when loading,
+        #  flip coords without transform
+        try:
+            x, y = consonant.final_shift
+            new_shape.attrib.update({'transform': f'translate({x} {y})'})
+        except TypeError:
+            new_shape.attrib.update(
+                {'transform': 'scale(-1 1) translate(-75 0)'})
+        return create_svg(abjad['base_final'], new_shape)
+    return create_medial_svg(consonant)
+
+
+abjad = {
+    'base_initial': Path(
         'M 2.5 0 '
         'V 57.5 '
         'A 15 15 0 0 0 17.5 72.5 '
         'H 75.0'),
-    'p': Path(
+    'base_medial': Path(
+        'M -12.5 72.5 '
+        'H 75'),
+    'base_medial_bar': Path(
+        'M 2.5 0 '
+        'V 57.5 '
+        'A 15 15 0 0 0 17.5 72.5 '
+        'H 75.0 '
+        'M 2.5 57.5 '
+        'A 15 15 0 0 1 -12.5 72.5'),
+    'base_final': Path(
+        'M 72.5 0 '
+        'V 57.5 '
+        'A 15 15 0 0 1 57.5 72.5 '
+        'H -12.5'),
+    'p': Consonant(Path(
         'M 22.5 55 '
         'V 35 '
         'A 17.5 17.5 0 0 1 57.5 35 '
-        'V 55'),
-    'b': Path(
+        'V 55')),
+    'b': Consonant(Path(
         'M 22.5 20 '
         'V 40 '
         'A 17.5 17.5 0 0 0 57.5 40 '
-        'V 20'),
-    't': Path(
-        'M 17.5 0 '
-        'V 55'),
-    'd': Path(
+        'V 20')),
+    't': Consonant(
+        Path(
+            'M 17.5 0 '
+            'V 55'),
+        medial_bar=True, final_bar=True),
+    'd': Consonant(Path(
         'M 20.0 57.5 '
-        'H 75.0'),
-    'k': Path(
-        'M 3.2322 1.7678 '
-        'L 31.9822 30.5177'),
-    'g': Path(
+        'H 75.0')),
+    'k': Consonant(
+        Path(
+            'M 3.2322 1.7678 '
+            'L 31.9822 30.5177'),
+        medial_bar=True, final_bar=True),
+    'g': Consonant(Path(
         'M 44.4822 43.0178 '
-        'L 73.2322 71.7677 '),
-    "'": Path(
-        'M 0.0 2.5 '
-        'H 30.0 '
-        'M 0.0 27.5 '
-        'H 30.0'),
-    'm': Path(
+        'L 73.2322 71.7677 ')),
+    "'": Consonant(
+        Path(
+            'M 0.0 2.5 '
+            'H 30.0 '
+            'M 0.0 27.5 '
+            'H 30.0'),
+        medial_bar=True, final_bar=True),
+    'm': Consonant(Path(
         'M 47.5 72.5 '
         'V 47.5 '
         'H 72.5 '
-        'V 72.5'),
-    'n': Path(
+        'V 72.5')),
+    'n': Consonant(Path(
         'M 15.0 55 '
         'H 65.0 '
         'M 25.0 37.5 '
-        'H 55.0'),
-    'f': Path(
-        'M 5.0 2.5 '
-        'A 22.5 22.5 0 0 1 27.5 25 '
-        'V 27.5 '
-        'H 0.0'),
-    'v': Path(
+        'H 55.0')),
+    'f': Consonant(
+        Path(
+            'M 5.0 2.5 '
+            'A 22.5 22.5 0 0 1 27.5 25 '
+            'V 27.5 '
+            'H 0.0'),
+        medial_bar=True, final_bar=True),
+    'v': Consonant(Path(
         'M 72.5 70 '
         'A 22.5 22.5 0 0 0 50.0 47.5 '
         'H 47.5 '
-        'V 70'),
-    's': Path(
-        'M 2.5 27.5 '
-        'H 27.5 '
-        'V 2.5 '
-        'H 2.5 '
-        'M 2.5 2.5 '
-        'L 47.5 47.5'),
-    'z': Path(
+        'V 70')),
+    's': Consonant(
+        Path(
+            'M 2.5 27.5 '
+            'H 27.5 '
+            'V 2.5 '
+            'H 2.5 '
+            'M 2.5 2.5 '
+            'L 47.5 47.5'),
+        medial_bar=True, final_bar=True),
+    'z': Consonant(Path(
         'M 47.5 72.5 '
         'V 47.5 '
         'H 72.5 '
         'V 72.5 '
         'M 72.5 72.5 '
-        'L 27.5 27.5'),
-    'x': Path(
-        'M 30.0 2.5 '
-        'H 2.5 '
-        'M 2.5 2.5 '
-        'L 47.5 47.5'),
-    'gh': Path(
+        'L 27.5 27.5')),
+    'x': Consonant(
+        Path(
+            'M 30.0 2.5 '
+            'H 2.5 '
+            'M 2.5 2.5 '
+            'L 47.5 47.5'),
+        medial_bar=True, final_bar=True),
+    'gh': Consonant(Path(
         'M 72.5 45 '
         'V 72.5 '
         'M 72.5 72.5 '
-        'L 27.5 27.5'),
-    'h': Path(
-        'M 72.5 72.5 '
-        'V 45 '
-        'A 8.2843 8.2843 0 0 0 58.3579 39.1421 '
-        'L 41.6421 55.8579 '
-        'A 8.2843 8.2843 0 0 1 27.5 50 '
-        'V 25 '
-        'M 30.0 2.5 '
-        'L 0.0 2.5'),
-    'r': Path(
+        'L 27.5 27.5')),
+    'h': Consonant(
+        Path(
+            'M 72.5 72.5 '
+            'V 45 '
+            'A 8.2843 8.2843 0 0 0 58.3579 39.1421 '
+            'L 41.6421 55.8579 '
+            'A 8.2843 8.2843 0 0 1 27.5 50 '
+            'V 25 '
+            'M 30.0 2.5 '
+            'L 0.0 2.5'),
+        medial_bar=True, final_bar=True),
+    'r': Consonant(Path(
         'M 12.5 42.5 '
         'A 20 20 0 0 0 52.5 42.5 '
         'A 20 20 0 0 0 12.5 42.5 '
         'M 18.3579 56.6421 '
         'L 46.6421 28.3579'),
-    'l': Path(
-        'M 3.2322 1.7678 '
-        'L 19.6082 18.143 '
-        'A 15 15 0 0 1 19.6082 39.357 '
-        'L 3.2322 55.7322'),
-    'j': Path(
+        medial_bar=True, final_bar=True, final_shift=(10, 0)),
+    'l': Consonant(Path(
+        'M 73.2322 71.7678 '
+        'L 56.857 55.3918 '
+        'A 15 15 0 0 0 35.643 55.3918 '
+        'L 19.2678 71.7678')),
+    'j': Consonant(Path(
         'M 72.5 72.5 '
         'V 52.5 '
         'H 40.0 '
         'M 57.5 35 '
         'H 22.5 '
         'M 40.0 17.5 '
-        'H 2.5'),
-    'w': Path(
-        'M 12.5 50 '
-        'A 12.5 12.5 0 0 0 37.5 50 '
-        'A 12.5 12.5 0 0 0 12.5 50'),
+        'H 2.5')),
+    'w': Consonant(
+        Path(
+            'M 12.5 50 '
+            'A 12.5 12.5 0 0 0 37.5 50 '
+            'A 12.5 12.5 0 0 0 12.5 50'),
+        medial_bar=True, final_bar=True),
+    'e': Path(
+        'M 56.75 18.25 '
+        'L 73.25 1.75 '),
     'i': Path(
         'M 57.5 0 '
         'V 17.5 '
@@ -270,8 +348,7 @@ abugida = Abugida({
         'M 37.5 72.5 '
         'A 35 35 0 0 0 2.5 37.5 '
         'M 47.751 27.249 '
-        'L 27.249 47.751')
-})
+        'L 27.249 47.751')}
 
 
 def create_all_glyphs():
@@ -284,38 +361,53 @@ def create_all_glyphs():
         'k', 'g', 'x', 'gh', "'",
         'h', 'r', 'l', 'j', 'w']
     vowels = [
-        'i', 'u', 'o', 'y', 'ø']
+        'e', 'i', 'u', 'o', 'y', 'ø']
 
-    subdir = 'abugida_svg'
+    subdir = 'abjad_svg'
     if not os.path.isdir(subdir):
         os.mkdir(subdir)
+
     i = 0
     for numeral in numerals:
         parts = ('0',)
         if numeral != '0':
             parts = parts + (numeral,)
         to_file(
-            abugida.create_svg(parts),
+            create_svg(*(abjad[part] for part in parts)),
             os.path.join(subdir, f'{i}_{numeral}.svg'))
         i += 1
-    to_file(
-        abugida.create_svg(('null',), w='18.75'),
-        os.path.join(subdir, f'{i}_null.svg'))
+    to_file(create_svg(), os.path.join(subdir, f'{i}_SPACE.svg'))
     i += 1
     for consonant in consonants:
         to_file(
-            abugida.create_svg(('base', consonant)),
-            os.path.join(subdir, f'{i}_{consonant}e.svg'))
+            create_initial_svg(abjad[consonant]),
+            os.path.join(subdir, f'{i}_{consonant.upper()}.svg'))
+        i += 1
+        to_file(
+            create_medial_svg(abjad[consonant]),
+            os.path.join(subdir, f'{i}_{consonant.upper()}.medial.svg'))
+        i += 1
+        to_file(
+            create_final_svg(abjad[consonant]),
+            os.path.join(subdir, f'{i}_{consonant.upper()}.final.svg'))
         i += 1
         for vowel in vowels:
             to_file(
-                abugida.create_svg(('base', consonant, vowel)),
-                os.path.join(subdir, f'{i}_{consonant}{vowel}.svg'))
+                create_initial_svg(abjad[consonant], abjad[vowel]),
+                os.path.join(
+                    subdir,
+                    f'{i}_{consonant.upper()}.{vowel.upper()}.svg'))
+            i += 1
+            to_file(
+                create_medial_svg(abjad[consonant], abjad[vowel]),
+                os.path.join(
+                    subdir,
+                    f'{i}_{consonant.upper()}.{vowel.upper()}.medial.svg'))
             i += 1
     for vowel in vowels:
         to_file(
-            abugida.create_svg((vowel,), min_x='55', w='20'),
-            os.path.join(subdir, f'{i}_{vowel}.svg'))
+            create_svg(abjad[vowel], min_x='55', w='20'),
+            os.path.join(subdir, f'{i}_{vowel.upper()}.svg'))
         i += 1
 
 
--- create_font.py
+++ create_font.py
@@ -2,71 +2,98 @@ import os
 
 import fontforge
 
-numerals = [
-    '0', '1', '2', '3', '4',
-    '5', '6', '7', '8', '9']
-numeral_strings = [
-    'ZERO', 'ONE', 'TWO', 'THREE', 'FOUR',
-    'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE']
-consonants = [
-    'p', 'b', 'm', 'f', 'v',
-    't', 'd', 'n', 's', 'z',
-    'k', 'g', 'x', 'gh', "'",
-    'h', 'r', 'l', 'j', 'w']
-vowels = [
-    'e', 'i', 'u', 'o', 'y', 'ø']
-
 path = os.path.dirname(__file__)
-svg_path = os.path.join(path, 'abugida_svg')
+svg_path = os.path.join(path, 'abjad_svg')
 
 new_font = fontforge.font()
 new_font.ascent = 1000
 new_font.descent = 0
 new_font.encoding = 'UnicodeFull'
+
+letters = []
 new_font.addLookup(
     'Composition Lookup',
     'gsub_ligature',
     None,
     (('ccmp', (('dflt', ('dflt',)),)),))
 new_font.addLookupSubtable('Composition Lookup', 'Compositions')
+new_font.addLookup(
+    'Medial',
+    'gsub_single',
+    None,
+    (('medi', (('dflt', ('dflt',)),)),))
+new_font.addLookupSubtable('Medial', 'Medial Forms')
+new_font.addLookup(
+    'Final',
+    'gsub_single',
+    None,
+    (('fina', (('dflt', ('dflt',)),)),))
+new_font.addLookupSubtable('Final', 'Final Forms')
+new_font.addLookup(
+    'Contextual Alternates',
+    'gsub_contextchain',
+    None,
+    (('calt', (('dflt', ('dflt',)),)),))
 
-i = 0
 code_point = 57344
-for numeral, numeral_string in zip(numerals, numeral_strings):
-    filename = os.path.join(svg_path, f'{i}_{numeral}.svg')
-    i += 1
-    code_point += 1
-    glyph = new_font.createChar(code_point, f'conlang{numeral_string}')
-    glyph.right_side_bearing = 1067
-    glyph.importOutlines(filename)
-filename = os.path.join(svg_path, f'{i}_null.svg')
-i += 1
-code_point += 1
-glyph = new_font.createChar(code_point, 'conlangNULL')
-glyph.width = 250
-glyph.right_side_bearing = 250 + 67
-glyph.importOutlines(filename)
-for consonant in consonants:
-    for vowel in vowels:
-        filename = os.path.join(svg_path, f'{i}_{consonant}{vowel}.svg')
-        i += 1
+
+files = sorted(os.listdir(svg_path), key=lambda x: int(x.split('_')[0]))
+char_names = [file.split('_')[-1].replace('.svg', '') for file in files]
+for i, file, char_name in zip(range(len(files)), files, char_names):
+    glyph_name = f'conlang{char_name}'
+    if char_name == 'SPACE':
+        # U+2003 Em Space
+        glyph = new_font.createChar(8195, glyph_name)
+    elif '.' in char_name:
+        glyph = new_font.createChar(-1, glyph_name)
+        # if not char_name.endswith(('.medial', '.final')):
+        #     consonant, vowel = char_name.split('.')
+        #     glyph.addPosSub(
+        #         'Compositions', (f'conlang{consonant}', f'conlang{vowel}'))
+    else:
+        glyph = new_font.createChar(code_point, glyph_name)
         code_point += 1
-        glyph = new_font.createChar(
-            code_point, f'conlang{consonant.upper()}{vowel.upper()}')
-        if vowel != 'e':
+
+    if not char_name.isnumeric() and not char_name == 'SPACE':
+        letters.append(glyph_name)
+        if not char_name.endswith(('.medial', '.final')):
+            if char_name + '.medial' in char_names:
+                glyph.addPosSub('Medial Forms', glyph_name + '.medial')
+            if char_name + '.final' in char_names:
+                glyph.addPosSub('Final Forms', glyph_name + '.final')
+
+    for vowel in ['E', 'I', 'U', 'O', 'Y', 'Ø']:
+        if vowel in char_name:
             glyph.addPosSub(
-                'Compositions', (
-                    f'conlang{consonant.upper()}E',
-                    f'conlang{vowel.upper()}'))
-        glyph.right_side_bearing = 1067
-        glyph.importOutlines(filename)
-for vowel in vowels:
-    if vowel != 'e':
-        filename = os.path.join(svg_path, f'{i}_{vowel}.svg')
-        i += 1
-        code_point += 1
-        glyph = new_font.createChar(code_point, f'conlang{vowel.upper()}')
-        glyph.importOutlines(filename)
+                'Compositions',
+                (glyph_name.replace('.' + vowel, ''), f'conlang{vowel}'))
+
+    glyph.importOutlines(os.path.join(svg_path, file))
+    glyph.left_side_bearing = 0
+    if char_name.isnumeric():
+        glyph.right_side_bearing = 67
+    elif char_name == 'SPACE':
+        glyph.right_side_bearing = 1000
+    else:
+        glyph.right_side_bearing = 0
+
+classes = (None, letters)
+new_font.addContextualSubtable(
+    lookup='Contextual Alternates',
+    subtable='Final Alternate Rule',
+    type='class',
+    rule=' 1 | 1 @<Final> | ',
+    bclasses=classes,
+    mclasses=classes,
+    fclasses=classes)
+new_font.addContextualSubtable(
+    lookup='Contextual Alternates',
+    subtable='Medial Alternate Rule',
+    type='class',
+    rule=' 1 | 1 @<Medial> | 1 ',
+    bclasses=classes,
+    mclasses=classes,
+    fclasses=classes)
 
 new_font.encoding = 'compacted'
 
